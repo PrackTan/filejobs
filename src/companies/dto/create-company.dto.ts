7011153fcac298ddce49f5929bd2ccf666c5b87a
unknown <nguyenchauhuutan@gmail.com>
2025-02-07 17:34:34 +0700

    update 7/2/2025

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCompanyDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+
+// CreateCompanyDto class is used to define the structure and validation rules for creating a new Company.
+export class CreateCompanyDto {
+    // The email field must be a valid email address.
+    @IsEmail(
+        {},{message: 'Tên công ty không đúng định dạng'}
+    )
+    name: string;
+
+
+    // The password field must not be empty and must have a minimum length of 8 characters.
+    @IsNotEmpty({message: 'Địa chỉ không được để trống'})
+    address: string;
+
+
+    description: string;
+
+}
--- src/companies/schema/company.schema.ts
+++ src/companies/schema/company.schema.ts
@@ -1,1 +1,47 @@
-export class Company {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+export type CompanyDocument = HydratedDocument<Company>;
+
+/// schema no-sql 
+/// entity sql
+@Schema({timestamps: true})
+export class Company {
+    @Prop({required: true})
+    name: string;
+    @Prop()
+    address: string;
+    @Prop({required: true})
+    phone: string;
+    @Prop()
+    email: string;
+    @Prop()
+    description: string;
+    @Prop()
+    website: string;
+    @Prop()
+    createBy: {
+        _id: string;
+        name: string;
+    };
+    @Prop()
+    updateBy: {
+        _id: string;
+        name: string;
+    };
+    @Prop()
+    deleteBy: {
+        _id: string;
+        name: string;
+    };
+    @Prop()
+    createdAt: Date;
+    @Prop()
+    updatedAt: Date;
+    @Prop()
+    isDeleted: boolean;
+    @Prop()
+    deletedAt: Date;
+
+}
+
+export const CompanySchema = SchemaFactory.createForClass(Company);
